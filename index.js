0bb55cde3aa7c3c30c71b2cce3568653e9c1f7d4
Indra Gunawan <feedsbrain@gmail.com>
2019-07-01 16:55:59 +0800

    Initial Release Version (#2)

--- index.js
+++ index.js
@@ -5,27 +5,30 @@ const pssh = require('pssh-tools')
 
 program
   .version('0.1.0', '-v, --version')
-  .option('-W, --widevine', 'Widevine Tools')
-  .option('-P, --playready', 'Playready Tools')
-  .option('-b, --base64', 'Output base64 encoded')
-  .option('-h, --hex', 'Output hexadecimal encoded')
-  .option('-H, --human', 'Output a human readable string')
-  .option('-p, --from-base64 [pssh-box]', 'Parse the given base64 encoded PSSH box')
-  .option('-d, --from-base64-data [pssh-box]', 'Parse the given base64 encoded PSSH box')
+  .option('-W, --widevine', 'Switch for Widevine')
+  .option('-P, --playready', 'Switch for Playready')
+  .option('-k, --b64-key [key]', 'Decode base64 PlayReady key')
+  .option('-p, --b64 [pssh-box]', 'Parse the given base64 encoded PSSH box (universal)')
+  .option('-d, --b64-data [pssh-data]', 'Parse the given base64 encoded PSSH data (combined with -W or -P switch)')
   .parse(process.argv)
 
-if (program.fromBase64) {
-  const result = pssh.tools.decodePssh(program.fromBase64)
+if (program.b64) {
+  const result = pssh.tools.decodePssh(program.b64)
   console.log(result.printPssh())
 }
 
-if (program.fromBase64Data && program.widevine) {
-  const result = pssh.widevine.decodeData(program.fromBase64Data)
+if (program.b64Key) {
+  const result = pssh.playready.decodeKey(program.b64Key)
   console.log(result)
 }
 
-if (program.fromBase64Data && program.playready) {
-  const result = pssh.playready.decodeData(program.fromBase64Data)
+if (program.b64Data && program.widevine) {
+  const result = pssh.widevine.decodeData(program.b64Data)
+  console.log(result)
+}
+
+if (program.b64Data && program.playready) {
+  const result = pssh.playready.decodeData(program.b64Data)
   console.log(result)
 }
 
