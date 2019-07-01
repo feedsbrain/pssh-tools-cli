609f9b4621915aa6b37f07e17d97c70279d30a58
Indra Gunawan <feedsbrain@gmail.com>
2019-07-01 16:55:01 +0800

    Update version

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const pssh = require('pssh-tools')
 
 program
   .version('0.1.0', '-v, --version')
-  .option('-W, --widevine', 'Access to Widevine tools')
-  .option('-P, --playready', 'Access tp Playready tools')
+  .option('-W, --widevine', 'Switch for Widevine')
+  .option('-P, --playready', 'Switch for Playready')
   .option('-k, --b64-key [key]', 'Decode base64 PlayReady key')
-  .option('-p, --b64 [pssh-box]', 'Parse the given base64 encoded PSSH box')
-  .option('-d, --b64-data [pssh-data]', 'Parse the given base64 encoded PSSH data')
+  .option('-p, --b64 [pssh-box]', 'Parse the given base64 encoded PSSH box (universal)')
+  .option('-d, --b64-data [pssh-data]', 'Parse the given base64 encoded PSSH data (combined with -W or -P switch)')
   .parse(process.argv)
 
 if (program.b64) {
