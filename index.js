fdce88f461d0e7a828253896a4a3daebb8af6cd0
Indra Gunawan <feedsbrain@gmail.com>
2019-07-16 15:33:01 +0800

    PlayReady Key Encode

--- index.js
+++ index.js
@@ -3,11 +3,16 @@
 const program = require('commander')
 const pssh = require('pssh-tools')
 
+const PR_TEST_KEY_SEED = 'XVBovsmzhP9gRIZxWfFta3VVRPzVEWmJsazEJ46I'
+
 program
   .version('0.1.0', '-v, --version')
   .option('-W, --widevine', 'Switch for Widevine')
   .option('-P, --playready', 'Switch for Playready')
   .option('-k, --b64-key [key]', 'Decode base64 PlayReady key')
+  .option('-e, --kid [key]', 'Encode hex kid for PlayReady')
+  .option('-c, --key [key]', 'Encode hex key for PlayReady')
+  .option('-K, --keySeed [key]', 'KeySeed for PlayReady key')
   .option('-p, --b64 [pssh-box]', 'Parse the given base64 encoded PSSH box (universal)')
   .option('-d, --b64-data [pssh-data]', 'Parse the given base64 encoded PSSH data (combined with -W or -P switch)')
   .parse(process.argv)
@@ -19,6 +24,11 @@ if (program.b64) {
 
 if (program.b64Key) {
   const result = pssh.playready.decodeKey(program.b64Key)
+  console.log(result)
+}
+
+if (program.kid) {
+  const result = pssh.playready.encodeKey({ kid: program.kid, key: program.key || undefined }, !program.key ? PR_TEST_KEY_SEED : undefined)
   console.log(result)
 }
 
