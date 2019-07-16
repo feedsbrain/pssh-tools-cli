85794ae9ac62ca0d2a0c2879fdd42c20ff5c0b6b
Indra Gunawan <feedsbrain@gmail.com>
2019-07-16 16:23:11 +0800

    Add ability to generate PRO from CLI

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ program
   .option('-K, --keySeed [key]', 'KeySeed for PlayReady key')
   .option('-p, --b64 [pssh-box]', 'Parse the given base64 encoded PSSH box (universal)')
   .option('-d, --b64-data [pssh-data]', 'Parse the given base64 encoded PSSH data (combined with -W or -P switch)')
+  .option('-r, --pro', 'Generate PlayReady PRO with given kid and key (optionally using key seed)')
+  .option('-h, --human', 'Convert output of base64 key to human readable hex format')
   .parse(process.argv)
+
+const base64ToHex = (base64String) => {
+  return Buffer.from(base64String, 'base64').toString('hex')
+}
 
 if (program.b64) {
   const result = pssh.tools.decodePssh(program.b64)
@@ -28,7 +34,25 @@ if (program.b64Key) {
 }
 
 if (program.kid) {
-  const result = pssh.playready.encodeKey({ kid: program.kid, key: program.key || undefined }, !program.key ? PR_TEST_KEY_SEED : undefined)
+  let result
+  let keyPair = { kid: program.kid, key: program.key || undefined }
+  let KeySeed = !program.key ? PR_TEST_KEY_SEED : undefined
+
+  result = pssh.playready.encodeKey(keyPair, !program.key ? PR_TEST_KEY_SEED : undefined)
+
+  if (program.pro) {
+    const payload = { keyPairs: [{ kid: pssh.playready.decodeKey(result.kid), key: pssh.playready.decodeKey(result.key) }], keySeed: KeySeed, compatibilityMode: true, dataOnly: true }
+    result = pssh.playready.encodePssh(payload)
+
+    if (result.dataObject) {
+      result = result.dataObject
+    }
+  } else {
+    if (program.human) {
+      result.kid = base64ToHex(result.kid)
+      result.key = base64ToHex(result.key)
+    }
+  }
   console.log(result)
 }
 
